d0ececf0d67ee5e1ecf8d10e39ed9bbaf3c2be63
Kelly Munro <kelly.munro@powershop.co.nz>
2017-01-19 14:21:20 +1300

    Most popular apertures

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,7 @@ class DashboardController < ApplicationController
     @popular_keywords = AgLibraryKeyword.popular_keywords(10)
     @recent_keywords = AgLibraryKeywordPopularity.recent_keywords(10)
     @paired_keywords = AgLibraryKeywordCooccurrence.paired_keywords(10)
+
+    @popular_apertures = AgHarvestedExifMetadata.popular_apertures(10)
   end
 end
--- app/models/ag_harvested_exif_metadata.rb
+++ app/models/ag_harvested_exif_metadata.rb
@@ -1,3 +1,33 @@
 class AgHarvestedExifMetadata < ApplicationRecord
   self.table_name = 'AgHarvestedExifMetadata'
+
+  def self.popular_apertures(limit)
+    frequencies = frequencies(apertures)
+
+    sorted = frequencies.sort_by { |aperture, frequency| frequency }
+
+    sorted[0..(limit - 1)].map do |aperture|
+      aperture.first
+    end
+  end
+
+  private
+
+  def self.frequencies(data)
+    frequencies = {}
+
+    data.each do |item|
+      if frequencies.has_key?(item)
+        frequencies[item] += 1
+      else
+        frequencies[item] = 1
+      end
+    end
+
+    frequencies
+  end
+
+  def self.apertures
+    AgHarvestedExifMetadata.order(aperture: :desc).pluck(:aperture)
+  end
 end
