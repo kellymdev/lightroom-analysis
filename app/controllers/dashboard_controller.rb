0d917d8a09102e8bacb6d654485516bd8cd6482d
Kelly Munro <kelly.munro@powershop.co.nz>
2017-01-20 12:44:47 +1300

    Additional styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,7 +23,7 @@ class DashboardController < ApplicationController
     @popular_focal_lengths_with_cameras = AgHarvestedExifMetadata.popular_focal_lengths_with_cameras(10)
     @popular_lenses_with_cameras = AgHarvestedExifMetadata.popular_lenses_with_cameras(10)
 
-    @popular_crop_sizes = AdobeImageDevelopSetting.popular_crop_sizes(10)
-    @popular_white_balances = AdobeImageDevelopSetting.popular_white_balances(10)
+    @popular_crop_sizes = AdobeImageDevelopSetting.popular_crop_sizes(5)
+    @popular_white_balances = AdobeImageDevelopSetting.popular_white_balances(5)
   end
 end
